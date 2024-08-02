3383c2c0a2af808b41091613c68bb3c32b5a5899
zaki <z.goumri@esi-sba.dz>
2024-08-02 10:41:41 +0100

    fix new  errors

--- src/app/components/bio.jsx
+++ src/app/components/bio.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const Bio = () => {
     
     const handleDownload = () => {
-        const link = document.createElement('a');
-        link.href = '/Zakaria Goumri.pdf'; 
-        link.download = '/zakariaGoumri.pdf'; 
+        const link = document.createElement("a");
+        link.href = "/Zakaria Goumri.pdf"; 
+        link.download = "/zakariaGoumri.pdf"; 
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
@@ -13,12 +13,12 @@ const Bio = () => {
    
    
     return (
-        <main className='flex justify-center mt-[8.403vw]'>
-         <div className='flex flex-col items-center'>
+        <main className="flex justify-center mt-[8.403vw]">
+         <div className="flex flex-col items-center">
          <img src="/assets/me.svg" alt="Hero image"  className="border rounded-full border-transparent  w-[150px] md:w-[231px] " />         
          <h1 className="text-[4.404vw] font-extrabold">zakaria Goumri!</h1>
-         <h2 className='md:text-[1.813vw]  text-zinc-500 font-semibold mb-[1.458vw] '>  I do code & <span className='bg-gradient-to-r from-violet-400 to-orange-400  text-transparent bg-clip-text '>chill</span>🍿</h2>
-         <p className='text-neutral-200 sm:w-[37vw] text-center mb-[1.458vw]'>Passionate Software Engineer with a focus on React development,
+         <h2 className="md:text-[1.813vw]  text-zinc-500 font-semibold mb-[1.458vw] ">  I do code & <span className="bg-gradient-to-r from-violet-400 to-orange-400  text-transparent bg-clip-text ">chill</span>🍿</h2>
+         <p className="text-neutral-200 sm:w-[37vw] text-center mb-[1.458vw]">Passionate Software Engineer with a focus on React development,
          dedicated to crafting elegant and user-friendly web applications</p>
          <button onClick={handleDownload} className="bg-black border border-white px-[3.056vw] py-[1.111vw] rounded-[5.208vw] mb-[1.458vw]">Download cv</button>
          </div>
--- src/app/components/contactMe.jsx
+++ src/app/components/contactMe.jsx
@@ -1,13 +1,14 @@
 import React from "react"
 import {contacts} from "../constantes"
 import Link from "next/link";
+import Image from "next/image";
 
 const ContactMe = () => {
     return (
         <main id="contact" className="mb-6">
-            <div className='flex flex-col items-center mt-[7.41vw] '>
+            <div className="flex flex-col items-center mt-[7.41vw] ">
          <p className="text-[4.404vw] font-extrabold ">CONTACT ME</p>
-         <p className='bg-gradient-to-r from-violet-400 to-orange-400  text-transparent bg-clip-text text-sm font-medium tracking-widest mb-4'>EXPLORE NOW</p>
+         <p className="bg-gradient-to-r from-violet-400 to-orange-400  text-transparent bg-clip-text text-sm font-medium tracking-widest mb-4">EXPLORE NOW</p>
          </div>
          <div className="flex items-center flex-col gap-8">
          <p className="w-[50%] text-center">As a passionate software engineer, I thrive on the intricate dance between logic and creativity.for business and services you'll find my contacts</p>
@@ -16,7 +17,7 @@ const ContactMe = () => {
             return(
               <li key={index}>
               <Link href={contact.link} target="_blank" rel={contact.rel} >
-                <img src={contact.path}  alt="contact" />
+                <Image src={contact.path}  alt="contact" />
                 </Link>
                 </li>
             )
