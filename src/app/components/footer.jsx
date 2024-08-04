e7dca594f19be94c923be3f77b4507102223f985
zaki <z.goumri@esi-sba.dz>
2024-08-04 19:58:10 +0100

    new h

--- src/app/components/footer.jsx
+++ src/app/components/footer.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
  const Footer = () => {
     return(
     <main className="flex flex-col items-center text-neutral-200">
-     <p className="flex font-medium text-xs ">Loosely designed in &nbsp; <Image src="/assets/figma.svg" height={11} width={7.33}/>&nbsp; Figma and coded in Visual Studio Code by yours truly. </p>
+     <p className="flex font-medium text-xs ">Loosely designed in &nbsp; <Image src="/assets/figma.svg" height={11} width={7.33} alt="figma logo"/>&nbsp; Figma and coded in Visual Studio Code by yours truly. </p>
      <p className=" font-medium text-xs">Built with Next.js
      and Tailwind CSS, All text is set in the Inter typeface.</p>
     </main>
